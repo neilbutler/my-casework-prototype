4f1b1eab4ac26b56ccedeeef8143898f4ed3d350
neilbutler <neil.butler@digital.landregistry.gov.uk>
2015-05-07 13:34:09 +0100

    finish story 2

--- app/server.py
+++ app/server.py
@@ -47,9 +47,9 @@ def case_list():
   data = json.load(json_data)
   return render_template('casework/case-list.html', data=data)
 
-@app.route('/casework/manage-LR0001')
-def case_detail():
-    json_data=open('app/static/data/LR0001.json', "r")
+@app.route('/casework/manage-<casenumber>')
+def case_detail(casenumber):
+    json_data=open('app/static/data/'+ casenumber+ '.json', "r")
     data = json.load(json_data)
     return render_template('casework/manage-case.html', data=data)
 
